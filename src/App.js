5339945d125c290b2805610bb2e58d4582c3a67a
unknown <paolo.velarde@mail.utoronto.ca>
2022-02-09 03:12:17 -0500

    removed section

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
       <Calories />
       <Cardio />
       <Graphs />
-      <WhySpotter />
+      {/* <WhySpotter /> */}
       <Transformation />
       <Footer />
     </Wrapper>
