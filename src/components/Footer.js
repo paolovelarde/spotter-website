e1dc80557bbad31559d7f1d4bbede97047203ca1
unknown <paolo.velarde@mail.utoronto.ca>
2022-03-05 17:03:49 -0500

    footer copy

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,18 +14,19 @@ const styles = {
 
 const Content = styled.div`
   display: flex;
-  flex-direction: column;
-  justify-content: center;
+  flex-direction: rpw;
+  justify-content: space-between;
+  align-items: center;
   height: 120px;
-  padding: 0 0 0 256px;
+  padding: 0 256px 0 256px;
   @media (max-width: 1660px) {
-    padding: 0 0 0 128px;
+    padding: 0 128px 0 128px;
   }
   @media (max-width: 1360px) {
-    padding: 0 0 0 64px;
+    padding: 0 64px 0 64px;
   }
   @media (max-width: 1080px) {
-    padding: 0 0 0 32px;
+    padding: 0 32px 0 32px;
   }
   @media (max-width: 900px) {
     padding: 32px 16px;
@@ -50,10 +51,11 @@ const Subtitle = styled.p`
   font-family: JostRegular;
   font-size: 16px;
   color: #fff;
-  line-height: 1.5em;
+  line-height: 1em;
   margin: 0;
-  letter-spacing: 2.5px;
+  letter-spacing: 1px;
   text-align: center;
+  opacity: 0.25;
   @media (max-width: 900px) {
     letter-spacing: -0.25px;
     font-size: 18px;
@@ -65,7 +67,8 @@ function Footer() {
     <Parallax strength={200} style={styles.parallax}>
       <Content>
         <Fade>
-          <Title>Coming Summer 2022</Title>
+          <Title>© 2022, Spotter</Title>
+          <Subtitle>By Paolo V.</Subtitle>
         </Fade>
       </Content>
     </Parallax>
