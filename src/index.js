37414f1e94a7f02d76455705e339c2e408e3c901
unknown <paolo.velarde@mail.utoronto.ca>
2022-03-05 16:02:16 -0500

    updated GA tracking id

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import '../src/fonts/Jost-Regular.ttf';
-import '../src/fonts/Jost-Bold.ttf';
-import '../src/fonts/Jost-Medium.ttf';
-import ReactGA from 'react-ga';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "../src/fonts/Jost-Regular.ttf";
+import "../src/fonts/Jost-Bold.ttf";
+import "../src/fonts/Jost-Medium.ttf";
+import ReactGA from "react-ga";
 
-const TRACKING_ID = 'G-K3ERQKFGTH';
+const TRACKING_ID = "UA-128003919-2";
 ReactGA.initialize(TRACKING_ID);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
